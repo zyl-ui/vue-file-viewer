bd038437811ae67ec6d54a5c920f7bebdb4560aa
abu3389 <294333196@qq.com>
2023-01-13 14:39:01 +0800

    fix: 修复微软方式浏览api的https兼容

--- packages/vendors/officeOnline/index.js
+++ packages/vendors/officeOnline/index.js
@@ -2,7 +2,7 @@
  * @Author: zhanghan
  * @Date: 2023-01-09 21:21:33
  * @LastEditors: zhanghan
- * @LastEditTime: 2023-01-13 11:45:06
+ * @LastEditTime: 2023-01-13 14:28:22
  * @Descripttion: 渲染微软在线office浏览容器
  */
 
@@ -11,9 +11,10 @@
  */
 export default function(url, target) {
   const iframe = document.createElement('iframe')
-  iframe.src =
-    'http://view.officeapps.live.com/op/view.aspx?src=' +
-    encodeURIComponent(url)
+  // 跟随文件的协议访问
+  iframe.src = `${
+    url.indexOf('https') > -1 ? 'https' : 'http'
+  }://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(url)}`
   iframe.style = 'border:0;height: 100%;width:100%'
   target.appendChild(iframe)
 }
